ad173852ee78ebb0acb6ff7f804ca15cffeb9e33
linylmfos <iqianbaidu@163.com>
2020-03-03 20:47:24 +0800

    vue中vue set用法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <input v-model.number="price" />
+      <button @click="batchUpdate">批量更新价格</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="course-list">
+      <div v-for="c in courses" :key="c.name">{{c.name}} -- ${{c.price}}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      price: 0,
+      course: '',
+      courses: [],
+      totalCount:0,
+      show: false
+    }
+  },
+  computed: {
+    total() {
+      return this.courses.length + "门";
+    }
+  },
+  async created() {
+    const courses = await this.getCourses();
+    this.courses = courses;
+  },
+  methods: {
+    getCourses() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([{
+            name: "web全站"
+          }, {
+            name: "web高级"
+          }])
+        }, 2000)
+      });
+    },
+    batchUpdate() {
+      this.courses.forEach(c => {
+        // Vue.set(target, propertyName, value)
+        Vue.set(c, 'price', this.price)
+      })
+    }
   }
 }
 </script>
